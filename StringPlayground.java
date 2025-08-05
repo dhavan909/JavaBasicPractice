65522500e044ca2ba8905b956494b82110e49634
SHAIK.DHAVAN <dhavansk989@gmail.com>
2025-08-05 20:50:53 +0530

    Add files via upload

---
+++ StringPlayground.java
@@ -0,0 +1,11 @@
+public class StringPlayground {
+    public static void main(String[] args) {
+        String s1 = "hello boys";
+        System.out.println(s1.length());
+        System.out.println(s1.toUpperCase());
+        System.out.println(s1.toLowerCase());
+        System.out.println(s1.charAt(2));
+        System.out.println(s1.indexOf(' '));
+        System.out.println(s1.concat("hello boys"));
+}
+}
